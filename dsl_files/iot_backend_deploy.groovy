4fcecb116f34a88aeaa78ad71244e592c9c1399d
Bhagirath <bhagirath009@hotmail.com>
2018-11-14 15:18:15 -0600

    Update iot_backend_deploy.groovy

--- dsl_files/iot_backend_deploy.groovy
+++ dsl_files/iot_backend_deploy.groovy
@@ -6,21 +6,20 @@ def repo= 'bhagirathgudi/iot-backend'
 
 job(basePath+"/defaultjob") {
   
-      stages {
-            stage('Cloning git') {
-                  steps {
-                        github repo
-                  }
-            }
-            stage('Bulding project') {
-                  steps {
-                        gradle{
-                            makeExecutable(true)
-                            tasks('clean build')      
-                        } 
-                       // artifacts
-                  }
-            }
-            
-      }
+  node {
+   def repo = 'bhagirathgudi/iot-backend'
+   stage('Cloning git') { // for display purposes
+      steps{
+			github repo
+	  }
+   }
+   stage('Build') {
+      steps{
+			gradle {
+				makeExecutable(true)
+				tasks('clean build')
+			}
+	  }
+   }
+}
 }
