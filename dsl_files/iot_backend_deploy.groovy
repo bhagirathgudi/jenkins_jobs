4ed099be301bdbd3e4a19b0364c03017fb34ec84
Bhagirath <bhagirath009@hotmail.com>
2018-11-14 16:53:20 -0600

    Update iot_backend_deploy.groovy

--- dsl_files/iot_backend_deploy.groovy
+++ dsl_files/iot_backend_deploy.groovy
@@ -16,5 +16,9 @@ job("$basePath/build") {
                   makeExecutable(true)
                   tasks('clean build')      
             } 
+            shell('ssh bhagirath93@40.121.143.10 "mkdir ~/JavaApp"')
+            shell('scp ./build/libs/iot-backend-0.0.1-SNAPSHOT.jar bhagirath93@40.121.143.10:~/JavaApp')
+            shell('ssh bhagirath93@40.121.143.10 "cd ~/JavaApp && java -jar iot-backend-0.0.1-SNAPSHOT.jar -p 8080" ')
+            
       }
 }
